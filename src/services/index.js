78a9431d5de4eb1ae2ac9405fd0f32f4f6389d95
Mateus Esteves <mateus.esteves1994@gmail.com>
2020-03-07 19:18:56 -0300

    Criação do service de posts

---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    PostsService: require('./posts.service')
+}
---
+++ src/services/posts.service.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const { db } = require('../database');
+const uuid = require('uuid').v4;
+
+function getAll(filters = {}) {
+    return db.get('posts')
+        .filter(filters)
+        .value();
+}
+
+function getById(id) {
+    return db.get('posts')
+        .find({ id })
+        .value();
+}
+
+function isPostAuthoredBy(postId, authorId) {
+    let post = db.get('posts')
+        .find({ id: postId })
+        .value();
+
+    return post != null && post.authorId === authorId;
+}
+
+function deleteById(id) {
+    db.get('posts')
+        .remove({ id })
+        .write();
+}
+
+function createPost(postData, authorId) {
+    let newPost = Object.assign({}, postData);
+    newPost.authorId = authorId;
+    newPost.id = uuid();
+
+    db.get('posts')
+        .create(newPost)
+        .write();
+
+    return newPost;
+}
+
+function updatePost(postData) {
+    let newPost = Object.assign({}, postData);
+    newPost.authorId = undefined;
+
+    db.get('posts')
+        .find({ id: postData.id })
+        .assign(newPost)
+        .write();
+}
+
+
+module.exports = {
+    getAll,
+    getById,
+    isPostAuthoredBy,
+    deleteById,
+    createPost,
+    updatePost
+};

fa1316c6dcc190fe841f9638685ba319cf6423d0
Mateus Esteves <mateus.esteves1994@gmail.com>
2020-03-07 19:45:03 -0300

    Refatoração

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,17 +3,15 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-router.post('/login', passport.authenticate('local'), login);
-function login(req, res) {
+router.post('/login', passport.authenticate('local'), (req, res) => {
     let user = Object.assign({}, req.user);
     user.password = undefined;
     res.status(200).send(user);
-}
+});
 
-router.post('logout', logout);
-function logout(req, res) {
+router.post('logout', (req, res) => {
     req.logout();
     res.status(200).send();
-}
+});
 
 module.exports = router;
